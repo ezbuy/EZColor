15815a70a68b944ad35bfee47737cad3df835dee
许赟 <xuyun@ezbuy.com>
2019-12-16 17:20:11 +0800

    Initial Commit

---
+++ EZColor/EZColor.h
@@ -0,0 +1,19 @@
+//
+//  EZColor.h
+//  EZColor
+//
+//  Created by xuyun on 2019/12/16.
+//  Copyright © 2019 ezbuy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EZColor.
+FOUNDATION_EXPORT double EZColorVersionNumber;
+
+//! Project version string for EZColor.
+FOUNDATION_EXPORT const unsigned char EZColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZColor/PublicHeader.h>
+
+
